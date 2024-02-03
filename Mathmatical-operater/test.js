da68a62ec76b233b3f0929871271cca5e4b90697
Prachi Kucheriya <prachikucheriya@gmail.com>
2024-02-04 00:09:21 +0530

    hi

---
+++ Mathmatical-operater/test.js
@@ -0,0 +1,12 @@
+let light = "green";
+if(light == "red"){
+  console.log("Stop");
+}
+else if(light == "yellow"){
+  console.log("Be Ready");
+}
+else if(light == "green"){
+  console.log("go");
+}else{
+  console.log("there is invalid color");
+}
