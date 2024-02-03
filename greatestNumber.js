92964517d5c5c2b86e1b4899d3e8a4bee72813dc
Prachi Kucheriya <prachikucheriya@gmail.com>
2024-02-04 00:04:07 +0530

    hello

---
+++ greatestNumber.js
@@ -0,0 +1,16 @@
+var a = 2
+var b = 15
+var c = 4
+
+if(a>b   &&   a>c)
+{
+console.log('a is greatest')
+ }
+else if(b > a  &&  b>c)
+{
+  console.log("b is greatest")
+}
+else
+{
+console.log("c is greatest")
+}
